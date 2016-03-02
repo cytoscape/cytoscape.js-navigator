628a609e01f24c503ad15dbeaddf4c35b3126c5a
Ant Kennedy <antjkennedy@gmail.com>
2016-03-02 15:04:56 +0000

    tidied and added some comments

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,13 @@
 'use strict';
 
 var WEBPACK = require('webpack');
-var WEBPACK_EXTRACT_TEXT = require('extract-text-webpack-plugin');
 var WEBPACK_COPY = require('copy-webpack-plugin');
+var WEBPACK_HTML = require('html-webpack-plugin');
 
 module.exports = makeConfig();
 
 function makeConfig() {
   var ENV = process.env.npm_lifecycle_event;
-  var BUILD_ENV = 'build';
   var DIST_ENV = 'dist';
 
   var config = { };
@@ -18,34 +17,28 @@ function makeConfig() {
     app: __dirname + '/cytoscape.js-navigator.js',
   };
 
-  // define the loaders for building
-  // config.module = {
-  //   preLoaders: [ ],
-  //   loaders: [
-  //     // javascript loader, we use the babel transpiler to allow
-  //     // development in modern javascript
-  //     {
-  //       test: 'cytoscape.js-navigator.js',
-  //       loader: 'raw',
-  //       exclude: /(node_modules|bower_components)/,
-  //     },
-  //   ],
-  // };
-
-
-  // define the output, empty if test
+  // define the output, append min if building the dist
   config.output = {
     path: './dist',
     filename: 'cytoscape-navigator' + (ENV === DIST_ENV ? '.min' : '')+ '.js',
     libraryTarget: 'umd',
   };
 
+  // ensure these are not packaged with the build
+  config.externals = {
+    jquery: 'jquery',
+    cytoscape: 'cytoscape'
+  };
+
+  // copy the css to the dist folder for completeness
   config.plugins = [new WEBPACK_COPY([{
     from: './cytoscape.js-navigator.css',
     to: 'cytoscape-navigator.css',
     toType: 'file'
   }])];
 
+
+  // if we are building the dist create the minifed version
   if (ENV === DIST_ENV) {
     config.plugins.push(
       new WEBPACK.optimize.UglifyJsPlugin()
